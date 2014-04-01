1aa29a2ec9fb54c16cd5e38fc491c828f0e6630c
domicode <domi@domidesign.ch>
2014-04-01 16:35:12 -0400

    got server running

---
+++ home.rb
@@ -0,0 +1,53 @@
+require "sinatra"
+
+class Contact
+
+  @@all_contacts = []
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all_contacts
+    @@all_contacts
+  end
+
+  def valid? 
+    if name.empty? or name == "first name"
+      false
+    else 
+      true
+    end
+  end
+
+end
+
+
+
+post "/contacts" do 
+  puts params
+  new_contact = Contact.new(params[:first_name])
+  if !new_contact.valid?
+    redirect to("/")# do something
+  end
+  Contact.all_contacts << new_contact
+  redirect to ("/contacts") #add new contacs
+end
+
+get "/contacts" do
+  @contacts = Contact.all_contacts
+  erb :contacts
+end
+
+# get "/:name" do
+#   puts params
+#   @name = params[:name].capitalize #make it an instance variable so that it's acceesible in the erb etemplate
+#   erb :name
+# end
+
+get "/"  do #specific routes go to the bottom, individual ones are on top, like if else statement
+  puts params
+  erb :index
+end
